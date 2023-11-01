1ba3502ede092ec101405366b568bcf0660971a7
dd3dd <jacky81810@gmail.com>
2023-11-01 15:06:13 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import Root, { loader as rootLoader } from "./routes/root";
+
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
